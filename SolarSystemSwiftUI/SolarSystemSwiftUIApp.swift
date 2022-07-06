09c20843c84bef7a16799e0ffc314c4d7c9d306e
ginaiscoding <rpaek88@gmail.com>
2022-07-06 13:18:54 -0700

    Initial Commit

---
+++ SolarSystemSwiftUI/SolarSystemSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarSystemSwiftUIApp.swift
+//  SolarSystemSwiftUI
+//
+//  Created by Regina Paek on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SolarSystemSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
